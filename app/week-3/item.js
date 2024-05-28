20860b7f1bd8c2b683271f20862769dc33f12123
chrisboyle3 <140367834+chrisboyle3@users.noreply.github.com>
2024-05-27 22:08:17 -0600

    meta title update

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <main className="flex w-1/4 justify-between items-center p-4 border rounded-lg">
+    <main className="flex w-1/4 justify-between items-center p-4 border rounded-lg bg-slate-600">
       <div>
         <h2 className="text-lg font-semibold">{name}</h2>
         <p className="text-sm text-yellow-600">{category}</p>
